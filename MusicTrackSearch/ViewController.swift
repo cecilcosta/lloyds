5cb77089ea85cd31402975a8a37d0330a11d01ee
Cecil Costa <eduardo@conglomo.es>
2020-01-25 16:14:01 +0000

    Initial Commit

---
+++ MusicTrackSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicTrackSearch
+//
+//  Created by Cecil Costa on 25/01/2020.
+//  Copyright © 2020 Cecil Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
